b2ccdc3b6af782ef0c7487027ee1f213beb091a5
mukeshravikmar <128395650+mukeshravikmar@users.noreply.github.com>
2024-08-28 14:07:55 +0530

    AWS pyspark.py

--- AWS pyspark.py
+++ AWS pyspark.py
@@ -1,3 +1,4 @@
+#Imports necessary libraries and modules for AWS Glue ETL job, including AWS Glue's transforms, utility functions, context, and job management classes.
 import sys
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
@@ -5,28 +6,28 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
-############################### Added by Carlos ###############################
 from awsglue.dynamicframe import DynamicFrame
 
-
+####Retrieves the job name passed as an argument to the Glue job
 ## @params: [JOB_NAME]
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
-
+##Initializes a SparkContext (sc) and a GlueContext (glueContext), which is necessary for running the AWS Glue job. Also, initializes a Spark session (spark) within the Glue context.
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
-job.init(args['JOB_NAME'], args)
+job.init(args['JOB_NAME'], args)  ##Creates a new Glue job object (job) and initializes it with the job name (JOB_NAME).
+
 ## @type: DataSource
 ## @args: [database = "db_youtube_raw", table_name = "raw_statistics", transformation_ctx = "datasource0"]
 ## @return: datasource0
 ## @inputs: []
-############################### Added by Darshil ###############################
-predicate_pushdown = "region in ('ca','gb','us')"
 
+predicate_pushdown = "region in ('ca','gb','us')"
+####Defines a predicate filter to only include rows where the "region" column is either 'ca', 'gb', or 'us'.
 datasource0 = glueContext.create_dynamic_frame.from_catalog(database = "db_youtube_raw", table_name = "raw_statistics", transformation_ctx = "datasource0", push_down_predicate = predicate_pushdown)
 
-## @type: ApplyMapping
+## @type: ApplyMapping  ##Applies schema mapping to the data in datasource0 DynamicFrame to ensure data types are correctly defined and compatible with the target destination
 ## @args: [mapping = [("video_id", "string", "video_id", "string"), ("trending_date", "string", "trending_date", "string"), ("title", "string", "title", "string"), ("channel_title", "string", "channel_title", "string"), ("category_id", "long", "category_id", "long"), ("publish_time", "string", "publish_time", "string"), ("tags", "string", "tags", "string"), ("views", "long", "views", "long"), ("likes", "long", "likes", "long"), ("dislikes", "long", "dislikes", "long"), ("comment_count", "long", "comment_count", "long"), ("thumbnail_link", "string", "thumbnail_link", "string"), ("comments_disabled", "boolean", "comments_disabled", "boolean"), ("ratings_disabled", "boolean", "ratings_disabled", "boolean"), ("video_error_or_removed", "boolean", "video_error_or_removed", "boolean"), ("description", "string", "description", "string"), ("region", "string", "region", "string")], transformation_ctx = "applymapping1"]
 ## @return: applymapping1
 ## @inputs: [frame = datasource0]
@@ -45,13 +46,11 @@ dropnullfields3 = DropNullFields.apply(frame = resolvechoice2, transformation_ct
 ## @args: [connection_type = "s3", connection_options = {"path": "s3://bigdata-on-youtube-cleansed-euwest1-14317621-dev/youtube/raw_statistics/"}, format = "parquet", transformation_ctx = "datasink4"]
 ## @return: datasink4
 ## @inputs: [frame = dropnullfields3]
-
-############################### Added by Darshil ###############################
-#MAKE SURE YOU COPY ONLY WHAT IS NEEDED
-
+###Reads data from the AWS Glue Data Catalog table raw_statistics in the db_youtube_raw database, applying the defined predicate filter.
+##Writes the transformed data to an S3 bucket in Parquet format, partitioning it by the region field.
 datasink1 = dropnullfields3.toDF().coalesce(1)
 df_final_output = DynamicFrame.fromDF(datasink1, glueContext, "df_final_output")
 datasink4 = glueContext.write_dynamic_frame.from_options(frame = df_final_output, connection_type = "s3", connection_options = {"path": "s3://de-on-youtube-cleansed-useast1-dev/youtube/raw_statistics/", "partitionKeys": ["region"]}, format = "parquet", transformation_ctx = "datasink4")
 
-############################### Added by Darshil ###############################
+##Commits the job, signaling that all steps in the ETL process have been completed successfully.
 job.commit()
